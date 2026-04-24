3b61ca3b2527190e7eb914eae025b5c878b4d106
Pixelboy79 <advancegamingf@gmail.com>
2026-04-24 07:36:53 +0530

    Refactor xmake.lua for Android and remove Windows code
    Removed Windows-specific configurations and added Dobby package for Android platform.

--- xmake.lua
+++ xmake.lua
@@ -1,26 +1,24 @@
 add_rules("mode.debug", "mode.release")
 
-if is_plat("windows") then
-    add_requires("detours v4.0.1-xmake.1")
-end
 add_requires("nlohmann_json v3.11.3")
+-- Tell Xmake to fetch Dobby automatically
+add_requires("dobby") 
 
 target("ForceCloseOreUI")
     set_kind("shared")
     add_files("src/**.cpp")
     add_includedirs("src")
+    
     set_languages("c++20")
     set_strip("all")
-    add_linkdirs("lib")
+    
     add_packages("nlohmann_json")
-    if is_plat("windows") then
-        add_packages("detours")
-        remove_files("src/api/memory/android/**.cpp","src/api/memory/android/**.h")
-        add_cxflags("/utf-8", "/EHa")
-        add_syslinks("Shell32")
-    elseif is_plat("android") then
-        remove_files("src/api/memory/win/**.cpp","src/api/memory/win/**.h")
+    
+    if is_plat("android") then
+        remove_files("src/api/memory/win/**.cpp", "src/api/memory/win/**.h")
         add_cxflags("-O3")
-        -- Preloader requirement completely removed
-        add_cxxflags("-DLLVM_TARGETS_TO_BUILD=\"ARM;AArch64;BPF\"")
+        
+        -- Link Dobby and Android's logging library to the build
+        add_packages("dobby") 
+        add_syslinks("log")
 end
